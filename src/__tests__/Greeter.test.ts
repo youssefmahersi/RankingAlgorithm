bc1f88022ea48246e58d6da16f58c64a3d541ddb
youssefmahersi <yousefmahersi0@gmail.com>
2022-03-13 14:26:20 +0100

    export fix

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,16 +1,19 @@
 import { RankingAlgorithm } from '../index';
-const rankingAlgorithm = new RankingAlgorithm(8,0.0002,[{
-  field:"like",
-  valuable: true,
-  typeOfAdd: "Multiplication",
-  ref: ""
-},{
-  field:"popularity",
-  valuable: false,
-  typeOfAdd: "",
-  ref: "like"
-}])
+const rankingAlgorithm = new RankingAlgorithm(8, 0.0002, [
+  {
+    field: 'like',
+    valuable: true,
+    typeOfAdd: 'Multiplication',
+    ref: '',
+  },
+  {
+    field: 'popularity',
+    valuable: false,
+    typeOfAdd: '',
+    ref: 'like',
+  },
+]);
 
 test('first second', () => {
-  expect(rankingAlgorithm.calc(12,13,0)).toBe(845000);
+  expect(rankingAlgorithm.calc(12, 13, 0)).toBe(845000);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,38 @@
-import { argType,TypeOfAdd } from "./types/types";
-export  class RankingAlgorithm {
-
-    constructor(public stretch:number,public startValue:number,public config:argType[]){
-
-    }
-    time(t:number):number{
-        const expo:number = -(t/this.stretch);
-        const exp:number = Math.exp(expo);
-        return 1 -exp+this.startValue;
-    }
-    calc(...sumProps:any):number{
-        let total: number=0;
-        let i:number=0;
-        for (const config of this.config) {
-            if (config.valuable === true) {
-                const refarg = this.config.find((arg) => arg.ref === this.config[i].field);
-                const refargIndex = this.config.findIndex((arg) => arg.ref === this.config[i].field);
-                if (refargIndex !== -1) {
-                    switch (config.typeOfAdd) {
-                        case "Sum": {
-                            total = total + (sumProps[i] + sumProps[refargIndex]);
-                            break;
-                        }
-                        case "Multiplication": {
-                            total = total + (sumProps[i] * sumProps[refargIndex]);
-                            break;
-                        }
-                    }
-                }else{
-                    total = total + sumProps[i];
-                }
-                
+import { argType, TypeOfAdd } from './types/types';
+export class RankingAlgorithm {
+  constructor(public stretch: number, public startValue: number, public config: argType[]) {}
+  time(t: number): number {
+    const expo: number = -(t / this.stretch);
+    const exp: number = Math.exp(expo);
+    return 1 - exp + this.startValue;
+  }
+  calc(...sumProps: any): number {
+    let total: number = 0;
+    let i: number = 0;
+    for (const config of this.config) {
+      if (config.valuable === true) {
+        const refarg = this.config.find((arg) => arg.ref === this.config[i].field);
+        const refargIndex = this.config.findIndex((arg) => arg.ref === this.config[i].field);
+        if (refargIndex !== -1) {
+          switch (config.typeOfAdd) {
+            case 'Sum': {
+              total = total + (sumProps[i] + sumProps[refargIndex]);
+              break;
             }
-            else {
-                total = total + sumProps[i];
+            case 'Multiplication': {
+              total = total + sumProps[i] * sumProps[refargIndex];
+              break;
             }
-            i++;
+          }
+        } else {
+          total = total + sumProps[i];
         }
-        const lastResult:number = total/this.time(sumProps[sumProps.length-1]);
-        return lastResult;
+      } else {
+        total = total + sumProps[i];
+      }
+      i++;
     }
-
+    const lastResult: number = total / this.time(sumProps[sumProps.length - 1]);
+    return lastResult;
+  }
 }
-
